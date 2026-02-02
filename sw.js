4dd74df0e584e349848f8d009bd8345c240fc3b1
Alexander78598745 <camisetasfutbol.aj13@gmail.com>
2026-02-02 09:24:48 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'atm-hub-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './Atletico_Madrid.png'
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Recuperar recursos
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
